dc0b316efe95710e212d044151995e46ef9c682e
lddurbin <l.d.durbin@gmail.com>
2023-11-05 15:19:55 +1300

    Functions docs

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,16 @@
+#' Check That Webpage Exists In Wayback Machine
+#'
+#' @param url A string containing the URL to the webpage.
+#'
+#' @return If request is successful (i.e. the request was successfully performed
+#'   and a response with HTTP status code <400 was recieved), an HTTP response;
+#'   otherwise throws an error.
+#'
+#' @export
+#'
+#' @examples
+#' # Check that the CRAN homepage is in the Wayback Machine:
+#' check_url("https://cran.r-project.org/")
 check_url <- function(url) {
   slug <- "http://archive.org/wayback/available?url="
 
