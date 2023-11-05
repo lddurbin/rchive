d3854b47f7244489407061f7cdaaeab54aee1ed3
lddurbin <l.d.durbin@gmail.com>
2023-11-05 15:39:04 +1300

    User-facing function and tests etc.

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,27 @@
-#' Check That Webpage Exists In Wayback Machine
+#' Perform a HTTP GET Request
+#'
+#' @param url A string containing a URL.
+#'
+#' @return If request is successful (i.e. the request was successfully performed
+#'   and a response with HTTP status code <400 was recieved), an HTTP response;
+#'   otherwise throws an error.
+#'
+#' @noRd
+#'
+#' @examples
+#' get_request("https://cran.r-project.org/")
+get_request <- function(url) {
+  req <- httr2::request(url) |>
+    httr2::req_perform()
+
+  return(req)
+}
+
+
+#' Check That A Webpage Exists In The Wayback Machine
 #'
 #' @param url A string containing the URL to the webpage.
+#' @param message Print friendly messages to the console? Default is `TRUE`.
 #'
 #' @return If request is successful (i.e. the request was successfully performed
 #'   and a response with HTTP status code <400 was recieved), an HTTP response;
@@ -11,11 +32,14 @@
 #' @examples
 #' # Check that the CRAN homepage is in the Wayback Machine:
 #' check_url("https://cran.r-project.org/")
-check_url <- function(url) {
-  slug <- "http://archive.org/wayback/available?url="
+check_url <- function(url, message = TRUE) {
+  wayback_url <- paste0("http://archive.org/wayback/available?url=", url)
 
-  req <- httr2::request(paste0(slug, url)) |>
-    httr2::req_perform()
+  req <- get_request(wayback_url)
+
+  if(req$status == 200 & message == TRUE) {
+    cli::cli_alert_success("This webpage has already been saved to the Wayback Machine.")
+  }
 
   return(req)
 }
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,13 +1,31 @@
-test_that("check_url function returns expected results", {
+test_that("get_request function returns expected results", {
   # Define a known URL for testing
-  test_url <- "http://www.example.com"
+  test_url <- "https://cran.r-project.org/"
 
   # Call the function
-  result <- check_url(test_url)
+  result <- get_request(test_url)
 
   # Test that the request was successful (status code 200)
   expect_equal(result$status_code, 200)
 
   # Test that the result is a response object
   expect_s3_class(result, "httr2_response")
+})
+
+
+test_that("check_url function returns expected results", {
+  # Define a known URL for testing
+  test_url <- "https://cran.r-project.org/"
+
+  # Call your function
+  result <- check_url(test_url, message = FALSE)
+
+  # Test that the request was successful (status code 200)
+  expect_equal(result$status_code, 200)
+
+  # Test that the result is a response object
+  expect_s3_class(result, "httr2_response")
+
+  # Optionally, test for the success message if you want to ensure that the cli alert is triggered
+  expect_message(check_url(test_url), "This webpage has already been saved to the Wayback Machine.")
 })
